f93bf943d0b3152169c4745c2cc4d27df032ab61
coiai <coiaichan@gmail.com>
2020-11-28 14:16:41 +0900

    リンクのついか

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+// win: alt + ↑↓
+function sakura_theme_setup(){
+    add_theme_support('post-thumbnails');    
+}
+add_action('after_setup_theme', 'sakura_theme_setup');
+
+function sakura_theme_link(){
+    // 決まりではないけどレスポンシブの一つの手段として...
+    // wp_is_mobile() でモバイルかの判別ができる
+    if(wp_is_mobile()){
+        wp_enqueue_style('sp-css', get_template_directory_uri() . '/css/style_sp.css');
+    }else{
+        wp_enqueue_style('pc-css', get_template_directory_uri() . '/css/style_pc.css');
+    }
+    wp_enqueue_script('common', get_template_directory_uri() . '/js/common.js', array(), '1.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'sakura_theme_link');
+
+//  hoge,hoge    hoge, hoge
